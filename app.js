d3aceb1850577a2fac95d0f5dfbe92899a8485f2
JPR278 <mahavigneshwari8@gmail.com>
2026-07-15 15:13:18 +0530

    task3

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+
+const User = require("./User");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+
+.connect("mongodb://127.0.0.1:27017/UserDB")
+
+.then(() => {
+    console.log("MongoDB Connected");
+})
+
+.catch((err) => {
+    console.log(err);
+});
+
+app.post("/users", async (request, response) => {
+
+    try {
+
+        const { name, email, password } = request.body;
+
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return response.status(400).json({
+                message: "Email already exists"
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new User({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        await newUser.save();
+
+        response.status(201).json({
+            message: "User Registered Successfully",
+            user: newUser
+        });
+
+    }
+
+    catch (err) {
+
+        response.status(500).json({
+            message: err.message
+        });
+
+    }
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
